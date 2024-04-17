9a6303ed01f1519b54ccbc6c2f99e9d639b654e6
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-04-17 18:16:48 +0800

    React Essentials
    Utilising usestate and additional prop to handle dynamic UI elements

--- React-Essentials-App/src/App.jsx
+++ React-Essentials-App/src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
   return (
     <div>
       <Header />
@@ -16,7 +22,48 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              onSelect={() => setSelectedTopic("components")}
+              isSelected={selectedTopic === "components"}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              onSelect={() => setSelectedTopic("jsx")}
+              isSelected={selectedTopic === "jsx"}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              onSelect={() => setSelectedTopic("props")}
+              isSelected={selectedTopic === "props"}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onSelect={() => setSelectedTopic("state")}
+              isSelected={selectedTopic === "state"}
+            >
+              State
+            </TabButton>
+          </menu>
+
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+        </section>
       </main>
     </div>
   );
--- React-Essentials-App/src/components/CoreConcept/CoreConcept.jsx
+++ React-Essentials-App/src/components/CoreConcept/CoreConcept.jsx
@@ -1,6 +1,6 @@
 import "./CoreConcept.css"
 
-export default function CoreConcept({image, title, description}) {
+export default function CoreConcept({ image, title, description }) {
     return (
       <li>
         <img src={image} alt={title}/>
