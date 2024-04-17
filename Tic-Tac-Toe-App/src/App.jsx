1ca859a1cd3d335e5a460580d1480c45d734b2ec
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-04-18 03:27:49 +0800

    Game Board and Player Components
    Creation of game board and player components.
    
    Utilising game board and current player states on parent component (App) to propagate the current active user information to both game board and player component.
    Game Board - use current active player to determine player symbol
    Player - use current active player to highlight player's turn

--- Tic-Tac-Toe-App/src/App.jsx
+++ Tic-Tac-Toe-App/src/App.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            onSelectSquare={handleSelectSquare}
+            isActive={activePlayer === "X"}
+          ></Player>
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            onSelectSquare={handleSelectSquare}
+            isActive={activePlayer === "O"}
+          ></Player>
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ Tic-Tac-Toe-App/src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={(event) => setPlayerName(event.target.value)}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={() => setIsEditing((isEditing) => !isEditing)}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
