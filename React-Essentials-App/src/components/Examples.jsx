586fe892af01358f7aa2bf77f2504543a2678015
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-04-17 23:47:18 +0800

    Indentation Correction

--- React-Essentials-App/src/components/Examples.jsx
+++ React-Essentials-App/src/components/Examples.jsx
@@ -37,15 +37,15 @@ export default function Examples() {
             </menu>
 
             {!selectedTopic ? (
-            <p>Please select a topic.</p>
+                <p>Please select a topic.</p>
             ) : (
-            <div id="tab-content">
-                <h3>{EXAMPLES[selectedTopic].title}</h3>
-                <p>{EXAMPLES[selectedTopic].description}</p>
-                <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-                </pre>
-            </div>
+                <div id="tab-content">
+                    <h3>{EXAMPLES[selectedTopic].title}</h3>
+                    <p>{EXAMPLES[selectedTopic].description}</p>
+                    <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                    </pre>
+                </div>
             )}
         </Section>
     );
