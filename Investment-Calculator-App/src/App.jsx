4328c68b463920a34c36293803cefffe19303fad
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-04-19 03:22:47 +0800

    Implemented UserInput Component
    States of all user inputs are elevated to App component, as values are required in both UserInput & Results (children) components.

--- Investment-Calculator-App/src/App.jsx
+++ Investment-Calculator-App/src/App.jsx
@@ -1,10 +1,26 @@
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+  });
+
+  function handleInputChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => ({
+      ...prevUserInput,
+      [inputIdentifier]: newValue,
+    }));
+  }
+
   return (
     <>
       <Header />
+      <UserInput userInput={userInput} onInputChange={handleInputChange}/>
     </>
   );
 }
