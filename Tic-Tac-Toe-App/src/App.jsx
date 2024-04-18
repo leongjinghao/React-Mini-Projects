53619902077cc19692a26d02c764f807d8d1031c
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-04-18 16:48:12 +0800

    Implement Logs component
    Logs the players' turn records.

--- Tic-Tac-Toe-App/src/App.jsx
+++ Tic-Tac-Toe-App/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
