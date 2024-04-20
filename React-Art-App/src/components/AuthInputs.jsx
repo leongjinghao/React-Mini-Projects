3b92f898034122f40f0c091874da542f4cb9c6b2
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-04-20 17:56:22 +0800

    Modularise Components
    Further modularise components and their respective CSS styling applied.

--- React-Art-App/src/components/AuthInputs.jsx
+++ React-Art-App/src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { styled } from "styled-components";
+import { Button, TextButton } from "./Buttons";
+import LabeledInput from "./LabeledInput";
 // import "./AuthInputs.css";
 
 const AuthInputsContainer = styled.div`
@@ -20,53 +22,10 @@ const ControlContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  // (props) = props.invalid ? "red" : "white" also works
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  border-color: ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
 const ActionContainer = styled.div`
   display: flex;
   justify-content: flex-end;
   gap: 1rem;
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
-
-const TextButton = styled.button`
-  color: #f0b322;
-  border: none;
 `;
 
 export default function AuthInputs() {
@@ -92,24 +51,20 @@ export default function AuthInputs() {
   return (
     <AuthInputsContainer>
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p>
+        <LabeledInput
+          label="Email"
+          type="email"
+          $invalid={emailNotValid}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        ></LabeledInput>
+        <LabeledInput
+          label="Password"
+          type="password"
+          $invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        ></LabeledInput>
       </ControlContainer>
       <ActionContainer>
         <TextButton type="button" className="text-button">
