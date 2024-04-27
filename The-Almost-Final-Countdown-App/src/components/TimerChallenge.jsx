2d57ad1f9aa988b58c789183bc05527449422925
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-04-28 02:39:37 +0800

    Implementing Result Modal
    Utilising React ref to expose open() function reference from child to parent component. Note that receiving (child) component has to use forwardRef to receive the ref.
    
    This pattern enable child component to manage both the HTML element used within and the actual implementation of certain behavior i.e.  modal is implemented using dialog HTML element and it is shown with .showModal() function.
    
    Thus, expose a universal open() function to parent component through ref, and need not care about the actual implementation on the child component. Thereby, achieving separation of concerns.

--- The-Almost-Final-Countdown-App/src/components/TimerChallenge.jsx
+++ The-Almost-Final-Countdown-App/src/components/TimerChallenge.jsx
@@ -1,15 +1,18 @@
 import { useRef, useState } from "react";
+import ResultModal from "./ResultModal.jsx";
 
 export default function TimerChallenge({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
   const timer = useRef();
+  const dialog = useRef();
 
   function handleStart() {
     setTimerStarted(true);
     timer.current = setTimeout(() => {
       setTimerStarted(false);
       setTimerExpired(true);
+      dialog.current.open();
     }, targetTime * 1000);
   }
 
@@ -20,20 +23,22 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired ? <p>You lost!</p> : ""}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={!timerStarted ? handleStart : handleStop}>
-          {!timerStarted ? "Start" : "Stop"}
-        </button>
-      </p>
-      <p className={!timerStarted ? "" : "active"}>
-        {!timerStarted ? "Timer inactive" : "Time is running..."}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="Lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={!timerStarted ? handleStart : handleStop}>
+            {!timerStarted ? "Start" : "Stop"}
+          </button>
+        </p>
+        <p className={!timerStarted ? "" : "active"}>
+          {!timerStarted ? "Timer inactive" : "Time is running..."}
+        </p>
+      </section>
+    </>
   );
 }
