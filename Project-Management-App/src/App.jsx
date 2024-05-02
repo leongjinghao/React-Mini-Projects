12c46d1a2609b851def0833b93ef72b0418a2a35
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2024-05-03 00:16:52 +0800

    Implement Initial Landing Page
    Landing page comprise of a side bar (consisting your created project), and the content portion (content pertaining the selected project, or inputs for creating new project)
    
    Initial implementation to display inputs element for user to create new projects.

--- Project-Management-App/src/App.jsx
+++ Project-Management-App/src/App.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined, // undefined represents initial state w/ no project
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevProjectsState) => {
+      return {
+        ...prevProjectsState,
+        selectedProjectId: null, // null represents adding project action
+      };
+    });
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {projectsState.selectedProjectId === undefined ? (
+        <NoProjectSelected onStartAddProject={handleStartAddProject} />
+      ) : (
+        <NewProject />
+      )}
+    </main>
   );
 }
 
